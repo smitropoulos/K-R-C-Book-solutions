8d68d166e1b306cabbe60db4a7805686a930bee4
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2017-09-28 20:18:40 +0300

    Initial Commit

---
+++ The C Programming Language Book/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  The C Programming Language Book
+//
+//  Created by Stefanos Mitropoulos on 28/09/2017.
+//  Copyright © 2017 Stefanos Mitropoulos. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
